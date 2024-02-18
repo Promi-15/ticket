b3e373385ce824f2f116b0193a88f73b26166d2d
Promi-15 <promii1217@gmail.com>
2024-02-18 08:01:54 -0800

    success part added

---
+++ calculation.js
@@ -0,0 +1,12 @@
+function addBackgroundColor(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.add("bg-green-600");
+}
+function setBg(){
+ 
+    let x =  document.getElementById("seat")
+    let value = x.value ;
+    console.log(value);
+
+   
+}
